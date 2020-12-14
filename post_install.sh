139bc8cf960aa94c00b4106438d976701c1da616
Will Ross <paxswill@paxswill.com>
2020-12-14 15:11:24 -0500

    Allow z2m to access serial ports and it's config files

--- post_install.sh
+++ post_install.sh
@@ -9,12 +9,19 @@ USER_NAME=zigbee2mqtt
 INSTALL_DIR=/opt/zigbee2mqtt
 
 
-pw useradd -u $USER_ID -n $USER_NAME -d /nonexistent -s /sbin/nologin -w no
+pw useradd \
+	-u $USER_ID \
+	-n $USER_NAME \
+	-d /nonexistent \
+	-s /sbin/nologin \
+	-w no \
+	-G dialer
 
 mkdir -p "$INSTALL_DIR"
 git clone "$GIT_URL" "$INSTALL_DIR"
 (cd "$INSTALL_DIR" && npm ci --production)
 mv "${INSTALL_DIR}/data" /usr/local/etc/zigbee2mqtt
+chown -R zigbee2mqtt /usr/local/etc/zigbee2mqtt
 
 sysrc -f /etc/rc.conf zigbee2mqtt_enable="YES"
 # Generate a new network encryption key on first run
